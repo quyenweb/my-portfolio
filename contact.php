6587e3f6b31205ebc81c88ecb326398c06aa7622
quyenweb <quyen.bui26@gmail.com>
2019-08-04 16:23:33 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,24 +1,43 @@
 <?php
- 
-// Data coming from the HTTP POST request
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
- 
+
+
+$EmailTo = "quyen.bui26@gmail.com";
 $email_from = '88design.online';
-$email_subject = 'Form Submission'
+$Subject = " contact message";
+$Name = Trim(stripslashes($_POST['name'])); 
 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
 
-$email_body = "";
-$email_body = $email_body . "Name: " . $name . "<br>";
-$email_body = $email_body . "Email: " . $visitor_email . "<br>";
-$email_body = $email_body . "Message: " . $message;
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
 
-$to = "quyen.bui26@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
 
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
 
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+// send email 
+$success = mail($EmailTo, $Subject, $Body, $Email);
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
 ?>
