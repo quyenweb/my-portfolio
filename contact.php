26636ba0f5c42bbaec92a2f164e7ee7d7db857af
quyenweb <quyen.bui26@gmail.com>
2019-08-04 16:06:27 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,24 @@
 <?php
+ 
+// Data coming from the HTTP POST request
 $name = $_POST['name'];
-$email = $_POST['email'];
+$visitor_email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "quyen.bui26@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
+ 
+$email_from = '88design.online';
+$email_subject = 'Form Submission'
+
+
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "<br>";
+$email_body = $email_body . "Email: " . $visitor_email . "<br>";
+$email_body = $email_body . "Message: " . $message;
+
+$to = "quyen.bui26@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
