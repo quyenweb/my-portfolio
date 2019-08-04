82bdf78196ed3924fc8a878d3f6ec60a65ae2ccd
quyenweb <quyen.bui26@gmail.com>
2019-08-04 15:43:37 +0700

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -6,18 +6,25 @@ $visitor_email = $_POST['email'];
 $message = $_POST['message'];
  
 $email_from = '88design.online';
-$email_subject = "Form Submission";
+$email_subject = 'Form Submission'
 
-$email_body = "User name: $name.\n".
-				"User Email: $visitor_email.\n".
-				"User Message: $message.\n";
 
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "<br>";
+$email_body = $email_body . "Email: " . $email . "<br>";
+$email_body = $email_body . "Message: " . $message;
 
 $to = "quyen.bui26@gmail.com";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
-// Lets send the mail
-mail($to,$email_subject,$email_body,$headers);
 
-header("Location: contact.html");
+
+
+
+// Lets send the mail
+if(mail($to,$email_subject,$email_body,$headers))
+	
+    echo "Sent";
+else
+    echo "Failed";
 ?>
